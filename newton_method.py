f90b7d31bbbb9a9a1eb646cd0e194b777d810e40
Gabriel <gabriel.mazer@outlook.com>
2024-04-08 17:16:47 -0300

    feat: extra 'criterio_parada'

--- newton_method.py
+++ newton_method.py
@@ -6,6 +6,13 @@ def f(x):
 def df(x):
     return round((3*np.power(x,2) - 9), 4)
 
+def criterio_parada(x0, x1):
+    if abs(x1) < 1:
+        max = 1
+    else:
+        max = abs(x1)
+    return round((abs(x1-x0)/max), 4)
+
 def newton_method(x0, tol):
     iteracao = 0
     
@@ -13,10 +20,16 @@ def newton_method(x0, tol):
         fx0 = f(x0)
         dfx0 = df(x0)
         
-        if abs(fx0) < tol:
-            break
+        if iteracao == 0:
+            if  abs(fx0) < tol:
+                break
+        else:
+            parada = criterio_parada(x0_aux, x0)
+            if abs(fx0) < tol or parada  < tol:
+                break
         
         print(iteracao, x0, abs(fx0),  "\n\n")
+        x0_aux = x0
         x0 = round(x0 - fx0 / dfx0, 4)
         iteracao += 1
     
