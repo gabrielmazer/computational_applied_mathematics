22b642143f6a73c2f4984a5076647bb571f68b19
Gabriel <gabriel.mazer@outlook.com>
2024-04-13 12:38:38 -0300

    quotation changed

--- bisection_method.py
+++ bisection_method.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def f(x):
-    return x**5 - (10/9)*x**3 + (5/21)*x
+    return x + 2*np.cos(x)
 
 def bissecao(a, b, tol):
     iteracao = 0
     xm = 0
     while True:
         print(f"iteração: {iteracao}  a: {a}  b: {b}")
-        xm = round((a + b) / 2.0, 5)
-        fxm = round(f(xm), 5)
+        xm = round((a + b) / 2.0, 4)
+        fxm = round(f(xm), 4)
         if abs(fxm) < tol:
             break
         elif f(a) * fxm < 0:
@@ -19,11 +19,11 @@ def bissecao(a, b, tol):
         print(f"xm: {xm}  fxm: {fxm} \n\n")
         iteracao += 1
     
-    xm = round((a + b) / 2.0, 5)
-    fxm = round(f(xm), 5)
+    xm = round((a + b) / 2.0, 4)
+    fxm = round(f(xm), 4)
     print(f"xm: {xm}  fxm: {fxm} \n\n")
     return xm
 
-# Intervalo [a, b] e precisão de 10^-4
-raiz = bissecao(-0.65, -0.52, 1e-4)
+# Intervalo [a, b] e precisão de 10^-2
+raiz = bissecao(-3, 0.5, 1e-2)
 print("A raiz aproximada é:", raiz)
--- false_position.py
+++ false_position.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def f(x):
-    return round(x - x * np.log(x), 4)
+    return round(x**5 - (10/9)*x**3 + (5/21)*x, 4)
 
 def falsa_posicao(a, b, tol):
     iteracao = 0
@@ -17,16 +17,16 @@ def falsa_posicao(a, b, tol):
             b = xm
         else:
             a = xm
-        print(xm, fxm, "\n\n")
+        print(f"xm: {xm}   fxm: {fxm}\n\n")
         iteracao += 1
     
     xm = round((a * fb - b * fa) / (fb - fa), 4)
     fxm = round(f(xm), 4)
-    print(xm, fxm, "\n\n")
+    print(f"xm: {xm}   fxm: {fxm}\n\n")
     return xm
 
 # Intervalo [a, b] e precisão de 10^-3
-a = 1
-b = 3.5
-raiz = falsa_posicao(1, 3.5, 1e-3)
+a = -0.3
+b = 0.25
+raiz = falsa_posicao(a, b, 1e-3)
 print("A raiz aproximada é:", raiz)
